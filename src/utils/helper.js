349092536a2ce768031554daea0c744fe0c32b1a
Vasutorn S <vasutorn.s@ku.th>
2017-11-29 11:40:09 +0700

    comolete all helper function

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,1 +1,144 @@
+import { config } from "chai/lib/chai";
+import { expectTypes, compareByInspect } from "chai/lib/chai/utils";
+
 export const plus = (x, y) => x + y;
+
+export const conv2subnet = n => {
+    var d = (0xffffffff << (32 - n)) >>> 0;
+    if(n === 0)
+    {
+        d = 0;
+    }
+    return d;
+}
+
+export const v42ip = ipv4 => {
+    var arrip = ipv4.split(".");
+    var ip = 0;
+    var multi = 1 << 24;
+    for (var i = 0; i < 4; i ++) {
+        ip += arrip[i] * (multi >> (i * 8));
+    }
+    return ip;
+}
+
+export const ip2v4 = ipv4 => {
+    let n1 = ipv4 & 0xff;
+    let n2 = (ipv4 >>> 8) & 0xff;
+    let n3 = (ipv4 >>> 16) & 0xff;
+    let n4 = (ipv4 >>> 24) & 0xff;
+    var ip = n4 + "." + n3 + "." + n2 + "." + n1;
+    return ip;
+}
+
+const map = { 'ANY': 0, 'A': 7, 'B': 15, 'C': 23};
+export const genSubnetClass = c =>{
+    const a = new Array(32 - map[c]).fill(0);
+    const b = a.map((number, index) => ip2v4(conv2subnet(index+1+map[c])) + "/" + (index+1+map[c]));
+    return b;
+}
+
+export const networkAdd = (ipv4,n) =>{
+    var ip = v42ip(ipv4);
+    var netIp = (ip & conv2subnet(n)) >>> 0;
+    return netIp;
+}
+
+export const usableip = (ipv4,n) => {
+    var netadd = networkAdd(ipv4,n);
+    var ip1 = netadd + 1;
+    var ip2 = netadd + (~conv2subnet(n)) - 1;
+    return {
+        data1: ip1,
+        data2: ip2
+    };  
+}
+
+export const broadcast = (ipv4,n) => {
+    var bcip = networkAdd(ipv4,n) + (~conv2subnet(n));
+    return bcip; 
+}
+
+export const totalhost = (n) => {
+    var totalhost = 1 << (32-n);
+    return totalhost;
+}
+
+export const usablehost = (n) => {
+    var usablehost = (1 << (32-n)) - 2;
+    return usablehost;
+}
+
+export const wildcard = (n) => {
+    var wc = (~conv2subnet(n));
+    return wc;
+}
+
+export const subnetClass = (n) => {
+    var c = '';
+    var s = 0;
+    if(n < 8){
+        c = 'Any';
+        s = 0;
+    }
+    else if(n < 16){
+        c = "A";
+        s = 8;
+    }
+    else if(n < 24){
+        c = "B";
+        s = 16;
+    }
+    else {
+        c = "C";
+        s = 24;
+    }
+    return {
+        ipclass: c,
+        classsize: s
+    };  
+}
+
+export const ipType = (ipv4) => {
+    var type = "Public";
+    if (((v42ip(ipv4) >= v42ip("10.0.0.0")) & ((v42ip(ipv4) <= v42ip("10.255.255.255"))))
+       | ((v42ip(ipv4) >= v42ip("172.16.0.0")) & ((v42ip(ipv4) <= v42ip("172.31.255.255"))))
+       | ((v42ip(ipv4) >= v42ip("192.168.0.0")) & ((v42ip(ipv4) <= v42ip("192.168.255.255"))))){
+        type = "Private";
+    }
+    return type;
+}
+
+export const hexIP = (ipv4) => {
+    var hexString = "";
+    var ip = v42ip(ipv4);
+    hexString = ip.toString(16);
+    return hexString;
+}
+
+export const allposibleIp = (ipv4,n) =>
+{
+    var allIP = new Array();
+    var size = subnetClass(n).classsize;
+    var startIp = networkAdd(ipv4,size);
+    //console.log(ip2v4(startIp));
+    var stopIp = broadcast(ipv4,size);
+    //console.log(ip2v4(stopIp));
+    var ip = startIp;
+    var i = 1;
+    allIP[0] = ip2v4(ip);
+     while (ip < stopIp)
+     {
+        allIP[i] =  ip2v4(usableip(ip2v4(ip),n).data1) + ' - ' + ip2v4(usableip(ip2v4(ip),n).data2);
+        i++;
+        allIP[i] =  ip2v4(broadcast(ip2v4(ip),n));
+        ip = broadcast(ip2v4(ip),n) + 1;
+        i++;
+        if(ip < stopIp)
+        {
+            allIP[i] = ip2v4(ip);
+            i++;
+        }
+     }
+    console.log(allIP);
+}
--- src/utils/helper.test.js
+++ src/utils/helper.test.js
@@ -1,5 +1,7 @@
 import { expect } from 'chai';
-import { plus } from './helper';
+import { plus, ip2v4, genSubnetClass, conv2subnet, networkAdd, usableip, broadcast, totalhost,
+         usablehost, wildcard, subnetClass, ipType, v42ip, hexIP ,allposibleIp } from './helper';
+import { config } from 'chai/lib/chai';
 
 describe('test plus', () => {
   it('should plus number', () => {
@@ -7,3 +9,95 @@ describe('test plus', () => {
     expect(plus(0, 2)).to.equal(2);
   })
 })
+
+describe('test ip2v4', () => {
+  it('subnet div bit test', () => {
+    expect(ip2v4(conv2subnet(0))).to.equal('0.0.0.0');
+    expect(ip2v4(conv2subnet(1))).to.equal('128.0.0.0');
+    expect(ip2v4(conv2subnet(2))).to.equal('192.0.0.0');
+    expect(ip2v4(conv2subnet(24))).to.equal('255.255.255.0');
+    expect(ip2v4(conv2subnet(32))).to.equal('255.255.255.255');
+  })
+})
+
+describe('test networkAdd', () => {
+  it('test networkAdd', () => {
+    expect(ip2v4(networkAdd('158.108.0.2',24))).to.equal('158.108.0.0');
+    expect(ip2v4(networkAdd('158.108.1.2',24))).to.equal('158.108.1.0');
+  })
+})
+
+describe('test usable', () => {
+  it('test usable', () => {
+    expect(ip2v4(usableip('158.108.0.2',24).data1) + ' - ' + ip2v4(usableip('158.108.0.2',24).data2)).to.equal('158.108.0.1 - 158.108.0.254');
+  })
+})
+
+describe('test broadcast', () => {
+  it('test broadcast', () => {
+    expect(ip2v4(broadcast('158.108.1.1',24))).to.equal('158.108.1.255');
+    expect(ip2v4(broadcast('158.108.12.34',28))).to.equal('158.108.12.47');
+  })
+})
+
+describe('test totalhost', () => {
+  it('test totalhost', () => {
+    expect(totalhost(28)).to.equal(16);
+    expect(totalhost(23)).to.equal(512);
+  })
+})
+
+describe('test usablehost', () => {
+  it('test usablehost', () => {
+    expect(usablehost(28)).to.equal(14);
+    expect(usablehost(23)).to.equal(510);
+  })
+})
+
+describe('test wildcard', () => {
+  it('test wildcard', () => {
+    expect(ip2v4(wildcard(28))).to.equal('0.0.0.15');
+    expect(ip2v4(wildcard(23))).to.equal('0.0.1.255');
+  })
+})
+
+describe('test wildcard', () => {
+  it('test wildcard', () => {
+    expect(ip2v4(wildcard(28))).to.equal('0.0.0.15');
+    expect(ip2v4(wildcard(23))).to.equal('0.0.1.255');
+  })
+})
+
+describe('test subnetClass', () => {
+  it('test subnetClass', () => {
+    expect(subnetClass(1).ipclass).to.equal('Any');
+    expect(subnetClass(26).ipclass).to.equal("C");
+  })
+})
+
+describe('test ipType', () => {
+  it('test ipType', () => {
+    expect(ipType("158.108.12.34")).to.equal('Public');
+    console.log(ipType("158.108.12.34"));
+    expect(ipType("192.168.1.1")).to.equal('Private');
+    console.log(ipType("192.168.1.1"));
+  })
+})
+
+describe('test hexIP', () => {
+  it('test hexIP', () => {
+    expect(hexIP("158.108.12.34")).to.equal('9e6c0c22');
+    expect(hexIP("158.108.22.34")).to.equal("9e6c1622");
+  })
+})
+
+describe('test allposibleIp', () => {
+  it('test allposibleIp', () => {
+    allposibleIp("123.2.0.0",18);
+  })
+})
+
+
+
+
+
